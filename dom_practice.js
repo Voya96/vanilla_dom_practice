876a70dc5253e04d63797a5d49554c0bfbfc4989
Voya96 <shavoya.beecher@gmail.com>
2020-09-07 01:02:54 -0400

    changed the innertext of h2 to have 3 exclamation marks at the end

--- dom_practice.js
+++ dom_practice.js
@@ -3,3 +3,4 @@ const myFoot = document.getElementById("myFooter")
 console.log(myFoot)
 const myHeader = document.querySelector("h2")
 console.log(myHeader)
+myHeader.innerText = "Welcome to Brooklyn!!!"
